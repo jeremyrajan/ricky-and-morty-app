58965b39344cd71204d4f5b2feb2e902d53507bc
Jeremy Rajan <jeremyrajan@gmail.com>
2020-01-10 12:08:06 +0800

    first draft 0.1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,21 +3,26 @@ import React, { useState, useEffect } from 'react';
 import List from '../../components/List';
 import axios from 'axios';
 
+import './style.scss';
+
 const HomePage = () => {
-  const [data, setData] = useState({characters: [], episodes: {}, info: {}});
+  const [data, setData] = useState({ characters: [], episodes: {}, info: {} });
+
+  const fetchData = async (page = 1) => {
+    page = page < 1 ? 1 : page;
+    const { data: { characters, episodes, info } } = await axios(`https://ricky-and-morty-api.now.sh/characters?page=${page}`);
+    setData({ characters, episodes, info, currentPage: page });
+  }
 
   useEffect(_ => {
-    const fetchData = async _ => {
-      const {data: {characters, episodes, info}} = await axios('https://ricky-and-morty-api.now.sh/characters');
-      setData({characters, episodes, info});
-    }
-
     fetchData();
   }, [])
 
   return (
     <div className="home">
+      <div className="ops prev-page" onClick={_ => fetchData(data.currentPage - 1)}>Previous</div>
       <List characters={data.characters} episodes={data.episodes} />
+      <div className="ops next-page" onClick={_ => fetchData(data.currentPage + 1)}>Next</div>
     </div>
   )
 }
